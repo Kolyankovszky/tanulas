9a94c5de11e3ab7f20f9615ff513f7ad65d00c74
Tanuló 301-02 <301-02@KKSZKI.LOCAL>
2021-11-16 18:06:29 +0100

    Masodik orai munka

--- teszt.py
+++ teszt.py
@@ -4,3 +4,6 @@ elsoSzam = int(input('Elso szám: '))
 masodikSzam = int(input('Masodik szam: '))
 print('Két szám összege: ', elsoSzam + masodikSzam)
 print('')
+szamSor = [3, 5, 11, 4, 15, 9]
+print(szamSor)
+print('')
