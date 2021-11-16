38a5eb83dfcfb961e2429f6cef88e227f877a7c6
Tanuló 301-02 <301-02@KKSZKI.LOCAL>
2021-11-16 18:28:47 +0100

    harmadik feladart

--- teszt.py
+++ teszt.py
@@ -5,5 +5,17 @@ masodikSzam = int(input('Masodik szam: '))
 print('Két szám összege: ', elsoSzam + masodikSzam)
 print('')
 szamSor = [3, 5, 11, 4, 15, 9]
+print(len(szamSor))
+osszeg = (sum(szamSor))
 print(szamSor)
+print('Összeg: ',osszeg)
+atlag = (osszeg/(len(szamSor)))
+print('Átlag: ',atlag)
+import random
+szamLista = []
+for i in range(10):
+    szamLista.append(random.randrange(1,10))
+
+for i in szamLista:
+    print(i)
 print('')
