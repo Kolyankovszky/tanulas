9cb782df78df446d3335ac8e4f6ad470ffa8b6f1
Tanuló 301-02 <301-02@KKSZKI.LOCAL>
2021-11-16 18:48:18 +0100

    negyedik feladat

--- teszt.py
+++ teszt.py
@@ -11,6 +11,17 @@ print(szamSor)
 print('Összeg: ',osszeg)
 atlag = (osszeg/(len(szamSor)))
 print('Átlag: ',atlag)
+min1 = min(szamSor)
+max1 = max(szamSor)
+print('Minimum: ', min1, 'Maximum: ', max1)
+vizsgalat = False
+for valtozo in szamSor:
+    if valtozo == 9:
+        vizsgalat = True
+if vizsgalat == True:
+    print('Tartalmaz 9 es számiot')
+else:
+    print('Nem tartalmaz 9 es számot')        
 import random
 szamLista = []
 for i in range(10):
